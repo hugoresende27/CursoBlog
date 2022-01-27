e4180873f4d648ff78e0a65f3d9869e72b06d1c5
hugoresende27 <Hugoresende27@gmail.com>
2022-01-27 10:37:03 +0000

    a

---
+++ cursoBlog/Modules/ModuloAlfa/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('moduloalfa::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('moduloalfa.name') !!}
+    </p>
+@endsection
