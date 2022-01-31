272f0de18c72016abac27743dd50bc9490a9e07e
hugoresende27 <Hugoresende27@gmail.com>
2022-01-31 17:39:05 +0000

    l

--- cursoBlog/resources/views/posts/show.blade.php
+++ cursoBlog/resources/views/posts/show.blade.php
@@ -55,28 +55,22 @@
                             {!! $post->body !!}
 
                        </div>
-                   </div>
+
+                       <section>
+                            <article>
+
+
+                            </article>
+                       </section>
+                   
+                    </div>
                </article>
            </main>
    
        </section>
   
    
-    {{-- <article>
-
-     <h1> {!!   $post->title; !!} </h1>
-
-     <p>
-        By <a href="../authors/{{ $post->author->username }}">{!!$post->author->name  !!}</a> in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }} </a>
-      </p>
-
-     <div>
-        {!! $post->body; !!}
-     </div>
-
-    </article>
-
-    <a href="/">Go back</a> --}}
+   
 
 
 </x-layout>
